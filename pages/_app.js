471fa20d081c716325792f7aaa8263768ba041ec
everEvergolw <1092547508@qq.com>
2024-07-22 23:08:44 +0100

    design:featured product

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
-import "@/styles/globals.css";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+  body{
+  
+    padding:0;
+    margin:0;
+    font-family: "Roboto", sans-serif;
+  }
+
+`;
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    
+    <>
+
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+    </Helmet>
+
+      <GlobalStyles />
+     <Component {...pageProps} /> 
+
+    </>
+    
+    
+  
+  
+  
+  
+  
+  );
 }
